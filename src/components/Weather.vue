c452c04d9736278b7583ff786b37d5f6d5472dc7
raccoon <qsx314@gmail.com>
2019-07-23 20:48:24 +0900

    add weather component

---
+++ src/components/Weather.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="weather-container">
+      <img src="/assets/images/sun-morning.png" />
+      <div class="contents">
+        <div class="main">
+          <div class="city">{{weather.city}}</div>
+          <div class="temp">{{weather.temp}}℃</div>
+        </div>
+        <div class="yesterday">어제{{weather.yesterday_temp}}</div>
+        <div class="rain-prop">강수확률 {{weather.pop}}%</div>
+        <div class="airpollution-container">
+          <div class="text">미세먼지</div>
+          <div class="air">{{weather.pm10}}</div>
+          <div class="text">초미세먼지</div>
+          <div class="air">{{weather.pm25}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Weather',
+
+  computed: {
+    weather() {
+      return this.$store.getters.weather;
+      mainmain;
+    },
+  },
+
+  created() {
+    this.$store.dispatch('setWeather');
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.weather-container {
+  display: flex;
+  height: 130px;
+
+  img {
+    width: 130px;
+    height: 130px;
+  }
+
+  .contents {
+    padding: 10px 30px;
+    font-size: 14px;
+
+    .main {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+      font-size: 22px;
+      font-weight: bold;
+
+      .city {
+        margin-right: 10px;
+      }
+    }
+  }
+
+  .airpollution-container {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,19 +1,104 @@
 <template>
-  <div>
-    <p>Hello world!</p>
+  <div class="container">
+    <div class="date-container">
+      <div class="date-wrapper">
+        <div class="date tody selected">오늘</div>
+      </div>
+      <div class="date-wrapper">
+        <div class="date tomorrow">내일</div>
+      </div>
+    </div>
+
+    <div class="main-contents">
+      <component :is="dispaly" />
+    </div>
+
+    <div class="select-mode">
+      <div class="text">시간대별</div>
+      <div class="mode-wrapper">
+        <div class="mode" @click="changeMode">강수</div>
+      </div>
+    </div>
+    <div class="location">현재 위치</div>
   </div>
 </template>
 
 <script>
+import Weather from '../components/Weather.vue';
+
 export default {
+  components: {
+    Weather,
+  },
+
   data() {
-    return {};
+    return {
+      dispaly: 'Weather',
+      date: 'today',
+    };
+  },
+
+  methods: {
+    changeMode() {
+      this.dispaly = 'RainProb';
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-p {
-  font-size: 20px;
+.container {
+  width: 400px;
+  border-top: 1px solid #f1f1f1;
+  border-bottom: 1px solid #f1f1f1;
+  font-family: 'Sunflower', sans-serif;
+}
+.date-container {
+  display: flex;
+  height: 30px;
+  border-bottom: 1px solid #f1f1f1;
+
+  .date-wrapper {
+    width: 200px;
+    height: 30px;
+
+    .date {
+      width: 50px;
+      height: 30px;
+      margin: 0 auto;
+      color: #f1f1f1;
+      font-size: 16px;
+      line-height: 30px;
+      text-align: center;
+      box-sizing: border-box;
+    }
+
+    .selected {
+      color: blue;
+      font-weight: bold;
+      border-bottom: 2px solid #0074d9;
+    }
+  }
+}
+.main-contents {
+  height: 130px;
+}
+.select-mode {
+  height: 30px;
+  line-height: 30px;
+  border-top: 1px solid #f1f1f1;
+
+  .text {
+    float: left;
+  }
+
+  .mode-wrapper {
+    float: right;
+  }
+}
+.location {
+  height: 30px;
+  line-height: 30px;
+  border-top: 1px solid #f1f1f1;
 }
 </style>
