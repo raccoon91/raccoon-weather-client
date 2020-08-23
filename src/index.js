c203bed78014ba4bdb906d337746f631c2f44327
raccoon91 <qsx314@gmail.com>
2020-08-23 12:50:11 +0900

    add scss file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
