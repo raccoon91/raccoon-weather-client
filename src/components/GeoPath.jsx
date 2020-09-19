38e6f46ad15589b1c25cc0f074af5fdaac5716c3
raccoon91 <qsx314@gmail.com>
2020-09-19 20:32:29 +0900

    add GeoPath climate api, legend, Card component

--- src/components/GeoPath.jsx
+++ src/components/GeoPath.jsx
@@ -1,22 +1,60 @@
-import React, { useRef, useEffect, useState } from "react";
-import { json, select, geoPath, geoMercator } from "d3";
-import { useResizeObserver } from "src/hooks";
+import React, { useRef, useEffect } from "react";
+import { toJS } from "mobx";
+import { useObserver } from "mobx-react";
+import { select, geoPath, geoMercator, scaleThreshold } from "d3";
+import { Card } from "src/components";
+import { useStores, useResizeObserver } from "src/hooks";
+import "./GeoPath.scss";
+
+const useWeatherStore = () => {
+  const { store } = useStores();
+
+  return useObserver(() => ({
+    getCurrentWeather: store.getCurrentWeather,
+    getGeoJson: store.getGeoJson,
+    getClimate: store.getClimate,
+    selectGeoCountry: store.selectGeoCountry,
+    geoJson: toJS(store.geoJson),
+    climate: toJS(store.climate),
+    selectedCountry: toJS(store.selectedCountry),
+  }));
+};
 
 const margin = { top: 10, right: 10, bottom: 10, left: 10 };
+const colorDomain = [0, 50, 100, 150, 200, 250];
+const colorRange = [
+  "#ffffff",
+  "#eff3ff",
+  "#bdd7e7",
+  "#6baed6",
+  "#3182bd",
+  "#08519c",
+];
 
 export const GeoPath = () => {
   const svgRef = useRef(null);
   const wrapperRef = useRef(null);
   const dimensions = useResizeObserver(wrapperRef);
-  const [geoData, setGeoData] = useState(null);
-  const [selectedCountry, setSelectedCountry] = useState(null);
+  const {
+    getCurrentWeather,
+    getGeoJson,
+    getClimate,
+    selectGeoCountry,
+    geoJson,
+    climate,
+    selectedCountry,
+  } = useWeatherStore();
 
   useEffect(() => {
-    json("./GeoChart.korea.geo.json").then(setGeoData);
-  }, []);
+    getCurrentWeather();
+    getClimate();
+    getGeoJson();
+  }, [getCurrentWeather, getGeoJson, getClimate]);
 
   useEffect(() => {
-    if (!geoData) return;
+    if (!geoJson || !climate) return;
+
+    console.log("rerender");
 
     const { width: innerWidth, height: innerHeight } =
       dimensions || wrapperRef.current.getBoundingClientRect();
@@ -26,45 +64,84 @@ export const GeoPath = () => {
 
     const svg = select(svgRef.current);
 
-    svg
-      .attr("overflow", "hidden")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("transform", `translate(${margin.left},${margin.top})`);
-
     const projection = geoMercator()
-      .fitSize([width, height], selectedCountry || geoData)
+      .fitSize([width, height], selectedCountry || geoJson)
       .precision(100);
 
     const pathGenerator = geoPath().projection(projection);
 
+    const colorScale = scaleThreshold()
+      .domain(colorDomain.slice(1))
+      .range(colorRange);
+
     svg
+      .attr("width", width)
+      .attr("height", height)
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    svg
+      .select(".countries")
       .selectAll(".country")
-      .data(geoData.features)
+      .data(geoJson.features)
       .join("path")
       .on("click", (feature) => {
-        setSelectedCountry(selectedCountry === feature ? null : feature);
+        selectGeoCountry(feature);
       })
       .attr("class", "country")
       .transition()
       .duration(1000)
       .attr("d", (feature) => pathGenerator(feature))
-      .attr("fill", "#eee")
-      .attr("stroke", "black");
+      .attr("fill", (data) => {
+        const city = data.properties.CTP_KOR_NM;
+        const values = climate[city];
 
-    svg
-      .selectAll(".label")
-      .data([selectedCountry])
+        if (values) {
+          return colorScale(values.rn1);
+        } else {
+          return "#fff";
+        }
+      });
+
+    const legends = svg
+      .select(".legend")
+      .selectAll(".legends")
+      .data(colorDomain)
+      .join("g")
+      .attr("class", "legends")
+      .attr("transform", (data, index) => `translate(0, ${index * 20})`);
+
+    legends
+      .selectAll(".legend-color")
+      .data((data) => [data])
+      .join("rect")
+      .attr("class", "legend-color")
+      .attr("fill", (data) => colorScale(data))
+      .attr("width", 20)
+      .attr("height", 20);
+
+    legends
+      .selectAll(".legend-text")
+      .data((data) => [data])
       .join("text")
-      .attr("class", "label")
-      .text((feature) => feature && feature.properties.CTP_KOR_NM)
-      .attr("x", 5)
-      .attr("y", 15);
-  }, [geoData, dimensions, selectedCountry]);
+      .attr("class", "legend-text")
+      .text((data) => data)
+      .attr("x", 30)
+      .attr("y", 20 - 4)
+      .attr("font-size", "12px");
+  }, [geoJson, dimensions, selectedCountry, climate, selectGeoCountry]);
+
+  const selectedCity = selectedCountry?.properties.CTP_KOR_NM || "전국";
 
   return (
-    <div ref={wrapperRef} className="svg-wrapper">
-      <svg ref={svgRef}></svg>
-    </div>
+    <Card title={selectedCity}>
+      <div ref={wrapperRef} className="geo-path-container">
+        <svg ref={svgRef}>
+          <g className="countries">
+            <path className="country" />
+          </g>
+          <g className="legend" />
+        </svg>
+      </div>
+    </Card>
   );
 };
