03bb52722eb1ef76670c1861865602ee10a2d10f
raccoon91 <qsx314@gmail.com>
2020-09-19 20:28:06 +0900

    add Header component & remove some Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import React from "react";
-import { Card, Status, GeoPath, GlobalAverageTemp } from "./components";
+import { Card, Header, GeoPath, GlobalAverageTemp } from "src/components";
 import "./App.scss";
 
 export const App = () => {
   return (
     <div className="app-container">
       <div className="app-top">
-        <Status title="위치" value="서울" subValue="구로구 구로동"></Status>
-        <Status title="온도" value="30" subValue="32"></Status>
-        <Status title="강수확률" value="20"></Status>
-        <Status title="습도" value="80"></Status>
-        <Status title="미세먼지" value="30"></Status>
+        <Header />
       </div>
 
       <div className="app-bottom">
         <div className="app-left">
           <div className="geo-section">
-            <Card title="전국">
-              <GeoPath />
-            </Card>
+            <GeoPath />
           </div>
           <div className="global-section">
-            <Card title="평균 기온">
-              <GlobalAverageTemp />
-            </Card>
+            <GlobalAverageTemp />
           </div>
         </div>
 
