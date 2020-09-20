9a936868ba5456e24d99eebcc284d083d6518fc6
raccoon91 <qsx314@gmail.com>
2020-09-21 00:13:55 +0900

    add type guard to useResiezeObserver

--- src/hooks/useResizeObserver.ts
+++ src/hooks/useResizeObserver.ts
@@ -1,10 +1,14 @@
 import { useState, useEffect, MutableRefObject } from "react";
 import ResizeObserver from "resize-observer-polyfill";
 
-export const useResizeObserver = (ref: MutableRefObject<HTMLDivElement>) => {
+export const useResizeObserver = (
+  ref: MutableRefObject<HTMLDivElement | null>
+) => {
   const [dimensions, setDimensions] = useState<DOMRectReadOnly | null>(null);
 
   useEffect(() => {
+    if (!ref.current) return;
+
     const observeTarget = ref.current;
     const resizeObserver = new ResizeObserver((entries) => {
       entries.forEach((entry) => {
