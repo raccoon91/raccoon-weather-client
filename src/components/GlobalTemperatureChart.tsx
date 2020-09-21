30cd666322223f6a7c747fe8a736e16a8e9f660d
raccoon91 <qsx314@gmail.com>
2020-09-21 18:40:48 +0900

    refactor GlobalTemperatureChart

---
+++ src/components/GlobalTemperatureChart.tsx
@@ -0,0 +1,83 @@
+import React, { FC, useRef, useEffect } from "react";
+import { useObserver } from "mobx-react";
+import { useStores, useResizeObserver } from "src/hooks";
+import { Card } from "src/components";
+import {
+  GradientLine,
+  Dot,
+  XAxis,
+  YAxis,
+  VerticalAxis,
+} from "src/components/chart";
+
+const useWeatherData = () => {
+  const {
+    store: { weatherStore },
+  } = useStores();
+
+  return useObserver(() => ({
+    getGlobalTemperature: weatherStore.getGlobalTemperature,
+    globalTempYearList: weatherStore.globalTempYearList?.slice(),
+    globalTempDataList: weatherStore.globalTempDataList?.slice(),
+    globalTempChartDataList: weatherStore.globalTempChartDataList?.slice(),
+  }));
+};
+
+export const GlobalTemperatureChart: FC = () => {
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const dimensions = useResizeObserver(wrapperRef);
+  const {
+    getGlobalTemperature,
+    globalTempYearList,
+    globalTempDataList,
+    globalTempChartDataList,
+  } = useWeatherData();
+
+  const width = dimensions?.width || 0;
+  const height = dimensions?.height || 0;
+
+  useEffect(() => {
+    getGlobalTemperature();
+  }, [getGlobalTemperature]);
+
+  return (
+    <Card title="온도">
+      <div
+        ref={wrapperRef}
+        style={{ height: "180px", padding: "10px 10px 25px 20px" }}
+      >
+        <svg width={width} height={height} overflow="visible">
+          <Dot
+            dotColor="#cccccc"
+            width={width}
+            height={height}
+            lineData={globalTempChartDataList}
+          />
+          <VerticalAxis
+            width={width}
+            height={height}
+            axisDataList={globalTempDataList}
+          />
+          <GradientLine
+            chartId="global-temp-chart"
+            startColor="#3182bd"
+            endColor="#de2d26"
+            width={width}
+            height={height}
+            lineData={globalTempChartDataList}
+          />
+          <XAxis
+            width={width}
+            height={height}
+            axisDataList={globalTempYearList}
+          />
+          <YAxis
+            width={width}
+            height={height}
+            axisDataList={globalTempDataList}
+          />
+        </svg>
+      </div>
+    </Card>
+  );
+};
