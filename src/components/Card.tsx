469857ef8e46790567b56543cb20efcfb63f9763
raccoon91 <qsx314@gmail.com>
2020-09-21 20:25:35 +0900

    add Card component option element

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,14 +3,16 @@ import "./Card.scss";
 
 interface ICardProps {
   title: string;
+  option?: JSX.Element;
   children?: JSX.Element;
 }
 
-export const Card: FC<ICardProps> = ({ title, children }) => {
+export const Card: FC<ICardProps> = ({ title, option, children }) => {
   return (
     <div className="card-container">
       <div className="card-title-wrapper">
         <h2 className="card-title">{title}</h2>
+        {option}
       </div>
       {children || null}
     </div>
