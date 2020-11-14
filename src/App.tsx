f3aabe9ec75be01808ca0528bb50f9a3cfa200b5
raccoon91 <qsx314@gmail.com>
2020-11-15 03:39:28 +0900

    test navigator contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Main, NoMatch } from "src/pages";
+import { Main, NoMatch, Test } from "src/pages";
 import "./App.scss";
 
 export const App: FC = () => {
@@ -10,6 +10,9 @@ export const App: FC = () => {
         <Switch>
           <Route exact path="/">
             <Main />
+          </Route>
+          <Route  path="/test">
+            <Test />
           </Route>
           <Route path="*">
             <NoMatch />
