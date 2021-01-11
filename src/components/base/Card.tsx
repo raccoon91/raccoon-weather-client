d582351a9ba2d57074257350dc12910cc952a9e7
raccoon91 <qsx314@gmail.com>
2021-01-11 13:52:56 +0900

    remove card component scss

--- src/components/base/Card.tsx
+++ src/components/base/Card.tsx
@@ -1,5 +1,17 @@
 import React, { FC } from "react";
-import "./Card.scss";
+import styled from "styled-components";
+import { FlexBox, H2 } from "./styled";
+
+const CardContainer = styled.div`
+  width: 100%;
+  padding: 0 10px;
+  background-color: #fff;
+  border: 1px solid #e6e9ed;
+`;
+
+const CardTitleWrapper = styled(FlexBox)`
+  border-bottom: 1px solid #e6e9ed;
+`;
 
 interface ICardProps {
   title: string;
@@ -9,12 +21,14 @@ interface ICardProps {
 
 export const Card: FC<ICardProps> = ({ title, option, children }) => {
   return (
-    <div className="card-container">
-      <div className="card-title-wrapper">
-        <h2 className="card-title">{title}</h2>
+    <CardContainer>
+      <CardTitleWrapper height="48px" justifyContent="space-between" padding="0 10px">
+        <H2 size="18px" weight="bold" color="#73879c">
+          {title}
+        </H2>
         {option}
-      </div>
+      </CardTitleWrapper>
       {children || null}
-    </div>
+    </CardContainer>
   );
 };
