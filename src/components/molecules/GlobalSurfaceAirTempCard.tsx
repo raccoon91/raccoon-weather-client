08d915447b121005f6c69964ffe20beeb4b1750c
raccoon91 <dev.beomseok@gmail.com>
2021-12-15 21:09:45 +0900

    modify ClimateDashboard layout

--- src/components/molecules/GlobalSurfaceAirTempCard.tsx
+++ src/components/molecules/GlobalSurfaceAirTempCard.tsx
@@ -8,7 +8,7 @@ interface IGlobalSurfaceAirTempCardProps {
 
 export const GlobalSurfaceAirTempCard: FC<IGlobalSurfaceAirTempCardProps> = ({ title }) => {
   return (
-    <Card w="100%" h="32rem" p="3rem 4rem">
+    <Card w="100%" h="100%" p="3rem 4rem">
       <Box h="1.6rem">
         <Title3 size="sm">{title}</Title3>
       </Box>
