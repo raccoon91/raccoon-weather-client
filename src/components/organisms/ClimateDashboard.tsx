24b3554c416b4277c1eb19b26dba2b2e3c22736c
raccoon91 <dev.beomseok@gmail.com>
2021-12-15 21:09:45 +0900

    modify ClimateDashboard layout

--- src/components/organisms/ClimateDashboard.tsx
+++ src/components/organisms/ClimateDashboard.tsx
@@ -9,24 +9,24 @@ export const ClimateDashboard: FC<IClimateDashboardProps> = ({ tempChartDataList
     <Box o="hidden auto" f="1" h="100%" p="4rem 6rem" bgc="skyBlue" btlr="3rem">
       <Navigation />
 
-      <Box w="100%" h="30rem" m="3rem 0 1rem">
-        <GlobalSurfaceAirTempCard title="기후 변화" />
-      </Box>
-
-      <Box fd="row" j="space-between" w="100%" h="64rem" m="1rem 0 0">
-        <Box w="calc(50% - 1rem)" h="64rem" m="0 1rem 0 0">
+      <Box fd="row" j="space-between" w="100%" h="60rem" m="3rem 0 1rem">
+        <Box w="calc(48% - 1rem)" h="60rem" m="0 1rem 0 0">
           <MapCard title="지역" />
         </Box>
 
-        <Box fd="column" j="space-between" w="calc(50% - 1rem)" h="100%" m="0 0 0 1rem">
-          <Box w="100%" h="38rem" m="0 0 1rem">
+        <Box fd="column" j="space-between" w="calc(52% - 1rem)" h="100%" m="0 0 0 1rem">
+          <Box w="100%" h="32rem" m="0 0 1rem">
             <TempClimateCard title="온도" datasets={tempChartDataList} />
           </Box>
 
-          <Box w="100%" h="24rem" m="1rem 0 0">
+          <Box w="100%" h="26rem" m="1rem 0 0">
             <RainClimateCard title="강수량" datasets={rainChartDataList} />
           </Box>
         </Box>
+      </Box>
+
+      <Box w="100%" h="24rem" m="1rem 0 0">
+        <GlobalSurfaceAirTempCard title="기후 변화" />
       </Box>
     </Box>
   );
