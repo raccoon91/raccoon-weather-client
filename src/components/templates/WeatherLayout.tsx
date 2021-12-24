61ca580c46bebd80c47f7a6c1398b01a1f225f53
raccoon91 <dev.beomseok@gmail.com>
2021-12-24 11:41:53 +0900

    add page background wrapper

--- src/components/templates/WeatherLayout.tsx
+++ src/components/templates/WeatherLayout.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import styled from "styled-components";
-import { Flex, Box } from "components/atoms";
+import { Box, Flex } from "components/atoms";
 import { Navigation } from "components/molecules";
 
 const DashboardWrapper = styled(Box)`
@@ -14,18 +14,20 @@ interface IWeatherLayoutProps {
 
 export const WeatherLayout: FC<IWeatherLayoutProps> = ({ current, dashboard }) => {
   return (
-    <Flex o="hidden" w="100vw" maxw="1920px" h="100vh" m="0 auto" bgc="blue">
-      <Box w="26rem" p="9rem 3rem 3rem">
-        {current}
-      </Box>
-
-      <DashboardWrapper w="calc(100% - 26rem)" p="4rem 0 0" bgc="skyBlue">
-        <Box h="2rem" m="0 10rem 2rem">
-          <Navigation />
+    <Box w="100vw" h="100vh" bgc="background">
+      <Flex o="hidden" w="100%" maxw="1920px" h="100%" m="0 auto" bgc="blue">
+        <Box w="26rem" p="9rem 3rem 3rem">
+          {current}
         </Box>
 
-        <Box h="calc(100% - 4rem)">{dashboard}</Box>
-      </DashboardWrapper>
-    </Flex>
+        <DashboardWrapper w="calc(100% - 26rem)" p="4rem 0 0" bgc="skyBlue">
+          <Box h="2rem" m="0 10rem 2rem">
+            <Navigation />
+          </Box>
+
+          <Box h="calc(100% - 4rem)">{dashboard}</Box>
+        </DashboardWrapper>
+      </Flex>
+    </Box>
   );
 };
